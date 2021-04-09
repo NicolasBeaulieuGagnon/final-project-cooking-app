baff0b2e00f8485b7d40649188aab2b58fd08e33
Nicolas Beaulieu-Gagnon <beaulieu_gagnon_nicolas@hotmail.com>
2021-04-09 10:01:29 -0400

    small structure changes

--- server/index.js
+++ server/index.js
@@ -5,10 +5,21 @@ const bodyParser = require("body-parser");
 const PORT = 31415;
 
 express()
+  .use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, HEAD, GET, PUT, POST, DELETE"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  })
   .use(morgan("tiny"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
 
   .listen(PORT, function () {
-    console.info("🌍 Listening on port " + server.address().port);
+    console.info("🌍 Listening on port ", PORT);
   });
